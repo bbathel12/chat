add4cd2a78645722a6add90d5c868b4d83de6df2
Brice Bathel <brice@bluebarracuda.(none)>
2015-08-04 19:52:02 -0500

    all the stuff

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="/chat/scripts.js"></script>
   <link rel="stylesheet" href="/chat/chat.css">
+    
